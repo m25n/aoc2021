ca972fa68a4403198142750c43b0d19aa04dd38a
Matthew Conger-Eldeen <mceldeen@gmail.com>
2021-12-01 21:53:07 -0700

    add code to select which days to run and document how to run

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# aoc2021
+# Advent of Code 2021
+
+## Prerequsites
+ * [The Haskell Tool Stack](https://haskellstack.org)
+
+## Running
+```
+stack run [dayID[ dayID[ dayID]]] # dayID is day01, day02, etc.
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
+import Control.Monad (forM_, when)
+import qualified Data.List as List
 import Lib
+import qualified System.Environment as Env
 
 main :: IO ()
-main = day01
+main = do
+  args <- Env.getArgs
+  when (List.null args) (putStrLn "usage: stack run [dayID[ dayID[ dayID]]]")
+  forM_ args $ \dayId -> do
+    putStrLn $ "running " ++ dayId ++ ":"
+    case dayId of
+      "day01" -> day01
+      _ -> putStrLn $ "error: unknown day " ++ dayId
+    putStrLn ""
